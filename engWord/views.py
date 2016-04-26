c4b624e950f2665a15c791a18cb5be4258750497
greenyant <wogud86@gmail.com>
2016-04-26 12:05:25 +0900

    mp3 base64 decoding in javascript.

--- engWord/views.py
+++ engWord/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import Context, RequestContext
 
 # Create your views here.
+def index(request):
+    variables = RequestContext(request, { })
+    return render_to_response('engWord/index.html', variables)
